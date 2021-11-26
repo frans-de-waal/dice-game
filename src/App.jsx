bd5851520e7943203322f61f62a953335f5b4881
Frans de Waal <fmdewaal@gmail.com>
2021-11-27 00:07:26 +0200

    theme and assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,16 @@
 import React, { useState } from 'react'
-import logo from './logo.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button
-            type="button"
-            onClick={() => setCount((oldCount) => oldCount + 1)}
-          >
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
+    <div className="app">
+      <header className="header">
+        <img
+          className="logo"
+          src="/dice-game/src/assets/logo-s.png"
+          alt="logo"
+        />
+        <h1>Dice Game</h1>
       </header>
     </div>
   )
