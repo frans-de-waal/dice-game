d57fe35588d8b1791418b6b82e5d4d9a6b99df2a
Frans de Waal <fmdewaal@gmail.com>
2021-11-27 00:09:56 +0200

    image fix

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,7 @@ function App() {
   return (
     <div className="app">
       <header className="header">
-        <img
-          className="logo"
-          src="/dice-game/src/assets/logo-s.png"
-          alt="logo"
-        />
+        <img className="logo" src="/src/assets/logo-s.png" alt="logo" />
         <h1>Dice Game</h1>
       </header>
     </div>
